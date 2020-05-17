aa2ad43fee7f0b53e4dedff44255fab37f70e9f8
Zheni Dimitrova <jeninidi@abv.bg>
2020-05-17 16:22:54 +0200

    test

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page/main-page.component';
+
+
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainPageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
